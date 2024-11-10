6f9d9e468a19f711101f2cfb3e458d815d624f1f
Antonious Stewart <antonious.stewart@gmail.com>
2024-11-10 16:39:48 -0500

    add expense logic and refactor

--- cmd/expense-tracker-cli/main.go
+++ cmd/expense-tracker-cli/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"githug.com/Antonious-Stewart/expense-tracker-cli/internal/flags"
 )
 
 func main() {
-	if err := flags.Root(os.Args[1:]); err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
+	flags.Root(os.Args)
 }
